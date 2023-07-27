42b8026046fee38ae9a0ebd3d7f49251eadab480
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2023-07-28 02:07:24 +0530

    static files fix

--- Recruitments/settings.py
+++ Recruitments/settings.py
@@ -126,7 +126,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
